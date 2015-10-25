95579ba29eb0c429d789a9102bf8ca1110b8e120
Stas Rudakou <stas@garage22.net>
2015-10-25 11:26:42 +0300

    Fix tests

--- tests/test_acceptance.py
+++ tests/test_acceptance.py
@@ -1,7 +1,7 @@
+import pytest
+
+
+@pytest.mark.django_db
 def test_homepage(client):
     response = client.get('/')
     assert response.status_code == 200
-
-
-def test_nothing():
-    pass
--- wagtrail/settings/dev.py
+++ wagtrail/settings/dev.py
@@ -3,7 +3,7 @@ from .base import *  # noqa
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-DJANGO_TEMPLATES['DEBUG'] = True
+DJANGO_TEMPLATES['OPTIONS']['debug'] = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '=bi%8p-j&&y5%4h8^oq-%(b@p&&wyu72b7qk4dlyvwo)!)wn20'
