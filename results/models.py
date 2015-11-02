4816e3ccccc505f134905d01baf3975ec34f97e5
davojta <damarah@gmail.com>
2015-11-02 05:20:59 +0300

    add styles to results

--- results/models.py
+++ results/models.py
@@ -125,8 +125,10 @@ class ResultsRelatedJuryMember(Orderable):
     )
 
     name = property(lambda self: self.jury_member.name)
+    modal_id = property(lambda self: self.jury_member.name.replace(' ', '_'))
     info = property(lambda self: self.jury_member.info)
     photo = property(lambda self: self.jury_member.photo)
+    country = property(lambda self: self.jury_member.country)
 
     panels = [
         PageChooserPanel('jury_member'),
