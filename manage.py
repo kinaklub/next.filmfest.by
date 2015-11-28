090f1c00b47df4939d41e3e346f438ff4ff25065
Stas Rudakou <stas@garage22.net>
2015-11-28 12:17:26 +0300

    Add/override manage.py commands: bash, test

--- manage.py
+++ manage.py
@@ -2,9 +2,20 @@
 import os
 import sys
 
+
+def main():
+    if sys.argv[1] == 'test':
+        import tox
+        sys.exit(tox.cmdline(sys.argv[2:]))
+    elif sys.argv[1] == 'bash':
+        from subprocess import call
+        sys.exit(call('/usr/bin/bash', *sys.argv[2:]))
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                              "filmfest.settings.dev")
+        from django.core.management import execute_from_command_line
+        execute_from_command_line(sys.argv)
+
+
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "filmfest.settings.dev")
-
-    from django.core.management import execute_from_command_line
-
-    execute_from_command_line(sys.argv)
+    main()
