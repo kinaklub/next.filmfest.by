3044ab001215a34105db8d82e422db8c7f823536
Stas Rudakou <stas@garage22.net>
2016-03-20 11:48:46 +0300

    Update related facts attr name in results model

---
+++ results/migrations/0013_update_related_facts_name.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import modelcluster.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('results', '0012_add_winner_model'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='resultsfact',
+            name='page',
+            field=modelcluster.fields.ParentalKey(related_name='related_facts', to='results.ResultsPage'),
+        ),
+    ]
