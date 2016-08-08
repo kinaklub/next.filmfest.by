0f0150ef0c6b056fb062b3aa80e84c02109a6a19
Anatoli Babenia <anatoli@rainforce.org>
2016-08-08 10:03:50 +0300

    Implement main menu (#29)
    Extract top navigation menu structure from wagtail Page tree.
    Extract link for root page from HomePage object.
    Detect current page in main menu and set style as `active`.
    Render only children that have "Show in menu" flag set.
    Show translated menu items by using `caption` if available.

--- cpm_generic/templatetags/cpm_generic_tags.py
+++ cpm_generic/templatetags/cpm_generic_tags.py
@@ -1,27 +1,39 @@
-import re
 
 from django import template
 from django.utils import translation
 from django.conf import settings
 
+from home.models import HomePage
+
 
 register = template.Library()
 
 
-@register.inclusion_tag('cpm_generic/tags/mainmenu.html')
-def mainmenu(request):
-    curr_lang = translation.get_language().split('-')[0]
-    full_path = request.get_full_path()
-    match = re.match(r'^/%s(?P<path>/.*)$' % curr_lang, full_path)
-    path = match.group('path') if match else full_path
+def get_menu_pages(full_path):
+    homepage = HomePage.objects.get(slug='home')
+    children = list(homepage.get_children().live().in_menu().specific())
+    return [(page, page.url == full_path) for page in [homepage] + children]
 
-    languages = [
+
+def get_language_paths(full_path):
+    curr_lang = translation.get_language().split('-')[0]
+    prefix = '/%s/' % curr_lang
+    path = full_path
+    if path.startswith(prefix):
+        path = path[len(prefix):]
+
+    return [
         (
             code,
             name,
-            '/%s%s' % (code, path),
+            '/%s/%s' % (code, path),
             code == curr_lang
         ) for code, name in settings.LANGUAGES
     ]
 
-    return {'languages': languages}
+
+@register.inclusion_tag('cpm_generic/tags/mainmenu.html')
+def mainmenu(request):
+    full_path = request.get_full_path()
+    return {'languages': get_language_paths(full_path),
+            'menupages': get_menu_pages(full_path)}
