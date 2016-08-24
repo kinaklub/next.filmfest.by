c9c6ae2a1491f742f0e23f9fcf2b915b74c12d5a
Anatoli Babenia <anatoli@rainforce.org>
2016-08-24 16:21:07 +0300

    Use demjson instead of jsonlint

---
+++ scripts/jsoncheck.py
@@ -0,0 +1,18 @@
+"""
+Checks JSON files for errors. Recursively scans current dir.
+
+requires: demjson
+"""
+
+import os
+
+import demjson
+
+linter = demjson.jsonlint()
+
+for root, dirs, files in os.walk('.'):
+  for name in files:
+    filename = os.path.join(root, name)
+    if filename.endswith('.json'):
+      #print(filename)
+      linter.main([filename])
