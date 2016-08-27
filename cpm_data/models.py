5d4afab6e0cbe54188d8f6e79179e8331eb554ae
Stas Rudakou <stas@garage22.net>
2016-08-28 01:09:37 +0300

    Add SeasonRelatedJuryMember and SeasonRelatedPartner models

--- cpm_data/models.py
+++ cpm_data/models.py
@@ -1,14 +1,18 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from wagtail.wagtailadmin.edit_handlers import FieldPanel
+from modelcluster.fields import ParentalKey
+from modelcluster.models import ClusterableModel
+from wagtail.wagtailadmin.edit_handlers import (FieldPanel, InlinePanel,
+                                                PageChooserPanel)
+from wagtail.wagtailcore.models import Orderable  # TODO: is this good?
 from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 
 from cpm_generic.constants import COUNTRIES
 from cpm_generic.models import TranslatedField
 
 
-class JuryMember(models.Model):
+class JuryMember(ClusterableModel):
     name_en = models.CharField(max_length=250)
     name_be = models.CharField(max_length=250)
     name_ru = models.CharField(max_length=250)
@@ -40,7 +44,34 @@ class JuryMember(models.Model):
     ]
 
 
-class Partner(models.Model):
+class SeasonRelatedJuryMember(Orderable):
+    page = ParentalKey('Season', related_name='related_jury_members')
+    jury_member = models.ForeignKey(
+        'cpm_data.JuryMember',
+        null=True,
+        blank=True,
+        related_name='+'
+    )
+
+    category_en = models.CharField(max_length=250, blank=True, default='')
+    category_be = models.CharField(max_length=250, blank=True, default='')
+    category_ru = models.CharField(max_length=250, blank=True, default='')
+    category = TranslatedField('category_en', 'category_be', 'category_ru')
+
+    name = property(lambda self: self.jury_member.name)
+    info = property(lambda self: self.jury_member.info)
+    photo = property(lambda self: self.jury_member.photo)
+    country = property(lambda self: self.jury_member.country)
+
+    panels = [
+        PageChooserPanel('jury_member'),
+        FieldPanel('category_en'),
+        FieldPanel('category_be'),
+        FieldPanel('category_ru'),
+    ]
+
+
+class Partner(ClusterableModel):
     name_en = models.CharField(max_length=250)
     name_be = models.CharField(max_length=250)
     name_ru = models.CharField(max_length=250)
@@ -65,7 +96,25 @@ class Partner(models.Model):
     ]
 
 
-class Season(models.Model):
+class SeasonRelatedPartner(Orderable):
+    page = ParentalKey('Season', related_name='related_partners')
+    partner = models.ForeignKey(
+        'cpm_data.Partner',
+        null=True,
+        blank=True,
+        related_name='+'
+    )
+
+    name = property(lambda self: self.partner.name)
+    link = property(lambda self: self.partner.link)
+    image = property(lambda self: self.partner.image)
+
+    panels = [
+        PageChooserPanel('partner'),
+    ]
+
+
+class Season(ClusterableModel):
     name_en = models.CharField(max_length=250)
     name_be = models.CharField(max_length=250)
     name_ru = models.CharField(max_length=250)
@@ -75,4 +124,6 @@ class Season(models.Model):
         FieldPanel('name_en'),
         FieldPanel('name_be'),
         FieldPanel('name_ru'),
+        InlinePanel('related_jury_members', label="Jury members"),
+        InlinePanel('related_partners', label="Partners"),
     ]
