3b7452b6ffc52a4bc21128ddb9b04a6839286fe0
Stas Rudakou <stas@garage22.net>
2016-08-28 00:51:17 +0300

    Add modeladmin classes for Season, JuryMember, Partner models

--- cpm_data/wagtail_hooks.py
+++ cpm_data/wagtail_hooks.py
@@ -1,13 +1,34 @@
 from wagtail.contrib.modeladmin.options import ModelAdmin, modeladmin_register
-from cpm_data.models import JuryMember
+from cpm_data.models import JuryMember, Partner, Season
 
 
-class MyPageModelAdmin(ModelAdmin):
-    model = JuryMember
-    menu_label = 'Jury'
-    menu_order = 200  # will put in 3rd place (000 being 1st, 100 2nd)
-    list_display = ('name', 'country')
+class SeasonModelAdmin(ModelAdmin):
+    model = Season
+    menu_label = 'Seasons'
+    menu_icon = 'date'
+    menu_order = 200
+    list_display = ('name_en', 'name_be', 'name_ru')
     search_fields = ('name_en', 'name_be', 'name_ru')
 
 
-modeladmin_register(MyPageModelAdmin)
+class JuryMemberModelAdmin(ModelAdmin):
+    model = JuryMember
+    menu_label = 'Jury'
+    menu_icon = 'group'
+    menu_order = 210
+    list_display = ('name_en', 'name_be', 'name_ru', 'country')
+    search_fields = ('name_en', 'name_be', 'name_ru')
+
+
+class PartnerModelAdmin(ModelAdmin):
+    model = Partner
+    menu_label = 'Partners'
+    menu_icon = 'grip'
+    menu_order = 220
+    list_display = ('name_en', 'name_be', 'name_ru', 'image')
+    search_fields = ('name_en', 'name_be', 'name_ru')
+
+
+modeladmin_register(SeasonModelAdmin)
+modeladmin_register(JuryMemberModelAdmin)
+modeladmin_register(PartnerModelAdmin)
