9bc758bd716c79bcc3fbf6105d201e768ad2419e
davojta <damarah@gmail.com>
2016-11-13 13:28:11 +0300

    add secret.js to store pass and login to website

--- scripts/films.json_to_next_raw_data/config.js
+++ scripts/films.json_to_next_raw_data/config.js
@@ -1,8 +1,9 @@
+const secret = require('./secret')
 module.exports = {
     domain: 'filmfest.by',
     submissions_api_path: 'ru/submission/api/submissions/?format=json',
-    user: 'test',
-    pass: 'test',
+    user: secret.user,
+    pass: secret.pass,
     submission_dir: './private/',
     public_dir: './public/'
 }
