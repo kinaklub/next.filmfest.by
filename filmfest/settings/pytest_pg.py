911404bf65a22c87c624779dc74b6632179a8567
davojta <damarah@gmail.com>
2016-11-28 15:25:29 +0300

    fix tests for postgre

--- filmfest/settings/pytest_pg.py
+++ filmfest/settings/pytest_pg.py
@@ -4,7 +4,9 @@ from .pytest import *  # noqa
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'filmfest',
+        'NAME': 'postgres',
         'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
