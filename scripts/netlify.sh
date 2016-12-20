d48303b1504abf089370b48fbd9abfd0d9cadac2
Anatoli Babenia <anatoli@rainforce.org>
2016-12-20 06:50:07 +0300

    wget.log stderr to stdout and serve static resources

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -20,13 +20,13 @@ export PYTHONUNBUFFERED=1
 .v/bin/python manage.py migrate
 
 echo --- [bootstrap] starting server in background
-.v/bin/python manage.py runserver 127.0.0.1:8000 &
+.v/bin/python manage.py runserver 127.0.0.1:8000 --insecure &
 sleep 5
 
 echo --- [bootstrap] grabbing site copy
 mkdir public
 cd public
-wget -m http://127.0.0.1:8000 --no-host-directories | tee wget.log
+wget -m http://127.0.0.1:8000 --no-host-directories 2>&1 | tee wget.log
 pwd
 ls -la
 
