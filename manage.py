ba8f46ec968d84ddb964872c2623dbcd52f5c242
Stas Rudakou <stas@garage22.net>
2017-01-08 14:50:20 +0300

    Add uwsgi as a dependency and add launch helper

--- manage.py
+++ manage.py
@@ -4,15 +4,39 @@ import sys
 
 
 def main():
-    if sys.argv[1:2] == ['test']:
+    settings_module = os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                                            "filmfest.settings.dev")
+    cmd = (sys.argv[1:2] + [None])[0]
+    args = sys.argv[2:]
+
+    if cmd == 'test':
         import tox
-        sys.exit(tox.cmdline(sys.argv[2:]))
-    elif sys.argv[1:2] == ['bash']:
+        sys.exit(tox.cmdline(args))
+    elif cmd == 'bash':
         from subprocess import call
-        sys.exit(call('/usr/bin/bash', *sys.argv[2:]))
+        sys.exit(call('/usr/bin/bash', *args))
+    elif cmd == 'launch':
+        from django.core.management import execute_from_command_line
+        execute_from_command_line(['manage.py', 'migrate'])
+
+        http_socket = (args[:1] + [':8000'])[0]
+        uwsgi_args = [
+            'uwsgi',
+            '--module', 'filmfest.wsgi',
+            '--env', 'DJANGO_SETTINGS_MODULE={}'.format(settings_module),
+            '--http-socket', http_socket,
+            '--master',
+            '--harakiri=120',
+            '--max-requests=1000',
+            '--vacuum',
+        ]
+        if sys.real_prefix:
+            # we are in a virtual env - let's take it into account
+            uwsgi_args.extend(['--home', sys.prefix])
+            os.execv(os.path.join(sys.prefix, 'bin', 'uwsgi'), uwsgi_args)
+        else:
+            os.execvp('uwsgi', uwsgi_args)
     else:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-                              "filmfest.settings.dev")
         from django.core.management import execute_from_command_line
         execute_from_command_line(sys.argv)
 
