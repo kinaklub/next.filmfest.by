be814d962d971228db791e0765ce9b04b389f1dc
Stas Rudakou <stas@garage22.net>
2017-03-28 00:09:17 +0300

    Add events.TimeTable page model

--- events/models.py
+++ events/models.py
@@ -3,12 +3,15 @@ from __future__ import unicode_literals
 from django.db import models
 
 from modelcluster.fields import ParentalKey
+from modelcluster.models import ClusterableModel
 from wagtail.wagtailcore.fields import RichTextField
 from wagtail.wagtailcore.models import Orderable, Page
 from wagtail.wagtailadmin.edit_handlers import FieldPanel, InlinePanel
+from wagtail.wagtailsearch import index
 
 from cpm_generic.models import TranslatedField
 from modeladminutils.edit_handlers import AdminModelChooserPanel
+from modeladminutils.models import SearchableManager
 from submissions.constants import SECTIONS
 
 
@@ -71,31 +74,58 @@ class FilmProgram(Page):
     ]
 
 
-class Event(Page):
-
-    # TODO: add season
-    starts_at = models.DateTimeField(db_index=True)
-    program = models.ForeignKey(FilmProgram, null=True, blank=True,
-                                on_delete=models.PROTECT)
-
+class Venue(index.Indexed, ClusterableModel):
     name_en = models.CharField(max_length=1000)
     name_be = models.CharField(max_length=1000)
     name_ru = models.CharField(max_length=1000)
     name = TranslatedField('name_en', 'name_be', 'name_ru')
 
-    description_en = RichTextField(default='')
-    description_be = RichTextField(default='')
-    description_ru = RichTextField(default='')
-    description = TranslatedField('description_en',
-                                  'description_be',
-                                  'description_ru')
-    content_panels = Page.content_panels + [
-        FieldPanel('starts_at'),
-        FieldPanel('program'),
+    address_en = models.CharField(max_length=1000)
+    address_be = models.CharField(max_length=1000)
+    address_ru = models.CharField(max_length=1000)
+    address = TranslatedField('address_en', 'address_be', 'address_ru')
+
+    objects = SearchableManager()
+
+    def __unicode__(self):
+        return self.name
+
+    panels = [
         FieldPanel('name_en'),
         FieldPanel('name_be'),
         FieldPanel('name_ru'),
-        FieldPanel('description_en'),
-        FieldPanel('description_be'),
-        FieldPanel('description_ru'),
+        FieldPanel('address_en'),
+        FieldPanel('address_be'),
+        FieldPanel('address_ru'),
+    ]
+
+
+class TimeTableEvent(Orderable):
+
+    page = ParentalKey('TimeTable', related_name='related_events')
+    program = models.ForeignKey(FilmProgram, null=True, blank=True,
+                                related_name='+')
+    starts_at = models.DateTimeField(db_index=True)
+    venue = models.ForeignKey(Venue, null=True, blank=True, related_name='+')
+
+    content_panels = Page.content_panels + [
+        FieldPanel('program'),
+        FieldPanel('starts_at'),
+        AdminModelChooserPanel('venue'),
+    ]
+
+
+class TimeTable(Page):
+
+    # TODO: add season
+    caption_en = models.CharField(max_length=250)
+    caption_be = models.CharField(max_length=250)
+    caption_ru = models.CharField(max_length=250)
+    caption = TranslatedField('caption_en', 'caption_be', 'caption_ru')
+
+    content_panels = Page.content_panels + [
+        FieldPanel('caption_en'),
+        FieldPanel('caption_be'),
+        FieldPanel('caption_ru'),
+        InlinePanel('related_events', label="Events"),
     ]
